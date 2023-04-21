2eaf831317bbcfdd7fdaad00d0bb805a88ff6ef2
ms919 <81754336+ms919@users.noreply.github.com>
2023-04-21 23:40:30 +0900

    v0.1.2.2

--- lib/nicker_pocker/command.rb
+++ lib/nicker_pocker/command.rb
@@ -16,20 +16,78 @@ module NickerPocker
       @options = options
 
       # 初期値設定
-      @options[:output] = './nicker_pocker/outputs/'
+      @options[:input] = './db/migrate/'
+      @options[:output] = './nicker_pocker/'
       @options[:format] = :csv
     end
 
     # 実行
     def run
+      return if Dir.glob("#{@options[:input]}*\.rb").empty?
+
+      @options[:existing_version] = existing_version
+      output(read_data)
+    end
+
+    private
+
+    # 既存のテーブル定義書のバージョンを取得
+    #
+    # @return [Integer]
+    def existing_version
+      return if Dir.glob("#{@options[:output]}*\.#{@options[:format]}").empty?
+
+      str = nil
+      File.open("#{@options[:output]}table_definition.csv") do |f|
+        r = /^version.*\d{4}_\d{2}_\d{2}_\d{6}/
+        while str.nil? do
+          str = r.match(f.readline(chomp: true))
+        end
+      end
+
+      return if str.nil?
+      str.to_s.slice(/\d{4}_\d{2}_\d{2}_\d{6}/).gsub('_', '').to_i
+    end
+
+    # 対象マイグレーションファイルを読み込み
+    #
+    # @return [Array]
+    def read_data
+      arr = []
+      Dir::foreach(@options[:input]) do |file_name|
+        next if skip_files?(file_name)
+
+        File.open(@options[:input] + file_name) do |f|
+          arr.push(f.readlines.each(&:strip!))
+        end
+      end
+
+      arr.flatten.map do |row|
+        row if /^t\..*|^def\s.*/.match(row)
+      end.compact
+    end
+
+    # 読み込みスキップ対象のファイル判定
+    #
+    # @params [String] file_name
+    # @return [Boolean]
+    def skip_files?(file_name)
+      ['.', '..'].include?(file_name) || file_name.to_i <= @options[:existing_version]
+    end
+
+    # 出力
+    #
+    # @params [Array] data_list
+    def output(data_list)
       # ディレクトリ作成（なければ）
       FileUtils.mkdir_p(@options[:output])
       # csv作成
       now = Time.now.strftime('%Y%m%d%H%M%S')
 
-      CSV.open("#{@options[:output]}#{now}.csv", 'w') do |csv|
+      CSV.open("#{@options[:output]}table_definition.csv", 'w') do |csv|
         csv << ['output time']
         csv << [now]
+        csv << data_list
       end
     end
   end
--- lib/nicker_pocker/version.rb
+++ lib/nicker_pocker/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module NickerPocker
-  VERSION = "0.1.1"
+  VERSION = "0.1.2.2"
 end
