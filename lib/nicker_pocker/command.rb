3f51b5e0c692f182f353d7fb890dfec727d581d3
ms919 <81754336+ms919@users.noreply.github.com>
2023-04-29 15:48:12 +0900

    対象絞り込み

--- lib/nicker_pocker/command.rb
+++ lib/nicker_pocker/command.rb
@@ -4,6 +4,9 @@ require 'fileutils'
 require 'csv'
 
 module NickerPocker
+
+  MIGRATE_METHODS = %w(create_table)
+
   class Command
     class << self
       def exec(options = {})
@@ -25,84 +28,80 @@ module NickerPocker
     def run
       return if Dir.glob("#{@options[:input]}*\.rb").empty?
 
-      @options[:existing_version] = existing_version
-      output(*read_data)
+      output(read_data)
     end
 
     private
-
-    # 既存のテーブル定義書のバージョンを取得
-    #
-    # @return [Integer]
-    def existing_version
-      return 0 if Dir.glob("#{@options[:output]}*\.#{@options[:format]}").empty?
-
-      str = nil
-      r = /^version.*\d{4}_\d{2}_\d{2}_\d{6}/
-
-      begin
-        File.open("#{@options[:output]}table_definition.csv") do |f|
-          while str.nil? do
-            str = r.match(f.readline(chomp: true))
-          end
-        end
-      rescue EOFError
-        return 0 if str.nil?
-        return str.to_s.slice(/\d{4}_\d{2}_\d{2}_\d{6}/).gsub('_', '').to_i
-      end
-    end
 
     # 対象マイグレーションファイルを読み込み
     #
     # @return [Array]
     def read_data
-      data_list = []
-      version = 0
+      temp_data_list = []
+
       Dir::foreach(@options[:input]) do |file_name|
-        next if skip_files?(file_name)
-        file_version = file_name.to_i
-        version = file_version if version < file_version
+        next if ['.', '..'].include?(file_name)
 
         File.open(@options[:input] + file_name) do |f|
-          data_list.push(f.readlines.each(&:strip!))
+          temp_data_list.push(f.readlines.each(&:strip!))
         end
       end
 
-      [
-        data_list,
-        version.to_s
-      ]
+      temp_data_list.flatten!.select! { |row| target_data?(row) }.compact!
+      data_list = necessary_data(temp_data_list)
+
+      # TODO：NickerPocker::MigrateMethodsに渡して整形
     end
 
-    # 読み込みスキップ対象のファイル判定
+    # 対象データ判定
     #
-    # @params [String] file_name
+    # @params [String] row
     # @return [Boolean]
-    def skip_files?(file_name)
-      ['.', '..'].include?(file_name) || file_name.to_i <= @options[:existing_version].to_i
+    def target_data?(row)
+      return true if /^t\..*|^def\s.*/.match(row)
+
+      MIGRATE_METHODS.each do |method_name|
+        return true if /#{method_name}/.match(row)
+      end
+
+      false
+    end
+
+    # 必要なデータを抽出
+    #
+    # @params [Array] data_list
+    # @return [Array]
+    def necessary_data(temp_data_list)
+      data_list = []
+      methods_contents = []
+
+      # 使いやすく整形
+      temp_data_list.each do |data|
+       if /^def\s.*/.match(data)
+          data_list << methods_contents if methods_contents.any?
+          methods_contents = []
+       end
+
+       methods_contents << data
+      end
+      data_list << methods_contents if methods_contents.any?
+
+      # 不要データを除外
+      data_list.reject! { |data| data.include?('def down') }
+      data_list.map { |data| data.reject { |x| /^def\s.*/.match(x) } }.reject(&:empty?)
     end
 
     # 出力
     #
     # @params [Array] data_list
-    # @params [String] version
-    def output(data_list, version)
+    def output(data_list)
       # ディレクトリ作成（なければ）
       FileUtils.mkdir_p(@options[:output])
 
       # csv作成
       CSV.open("#{@options[:output]}table_definition.csv", 'w') do |csv|
-        csv << output_version(version)
         csv << data_list
       end
-    end
-
-    # 出力用バージョン取得
-    #
-    # @params [String]
-    # @return [Array]
-    def output_version(version)
-      ["version: #{version[0..3]}_#{version[4..5]}_#{version[6..7]}_#{version.split(/^\d{8}/).last}"]
     end
   end
 end
