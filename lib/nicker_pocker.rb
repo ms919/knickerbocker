23d930694c4895ecbcdd1f44f1e53da8bc9aa684
ms919 <81754336+ms919@users.noreply.github.com>
2023-06-13 07:29:05 +0900

    v0.2.3

--- lib/nicker_pocker.rb
+++ lib/nicker_pocker.rb
@@ -5,7 +5,4 @@ require_relative 'nicker_pocker/format/detail'
 require_relative 'nicker_pocker/command'
 require_relative 'nicker_pocker/grouping'
 
-module NickerPocker
-  class Error < StandardError; end
-  # Your code goes here...
-end
+module NickerPocker;end
